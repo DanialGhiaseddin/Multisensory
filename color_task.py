48d335e6affca40ce6a16c07684005c285ce7265
Danial Ghiaseddin <danial.ghiaseddin@gmail.com>
2024-06-23 21:43:20 -0400

    faeture updates on color task

--- color_task.py
+++ color_task.py
@@ -1,7 +1,6 @@
 from screeninfo import get_monitors
 import yaml
 from psychopy import visual, core, event, data, logging, gui, os
-import matplotlib.colors as mpl_colors
 
 import random
 from psychopy import visual
@@ -42,21 +41,54 @@ class ColorTask:
         self.win = visual.Window(size=self.config["screen"]["size"], fullscr=self.config["screen"]["full-screen"],
                                  units='height')
 
-        self.color_bar = self.CircularColorBar(self.win)
+        self.color_bar = self.CircularColorBar(self.win, radius=self.config["color_bar"]["radius"],
+                                               feedback_radius=self.config["stimulus"]["size"],
+                                               number_of_colors=self.config["color_bar"]["number_of_colors"],
+                                               slider_radius=self.config["color_bar"]["slider_radius"],
+                                               saturation=self.config["color_bar"]["saturation"],
+                                               value=self.config["color_bar"]["value"],
+                                               is_colorful=self.config["color_bar"]["colorful"]
+                                               )
 
         # Define stimuli
-        self.instruction_text = visual.TextStim(self.win, text="Press any key to start.")
-        self.fixation = visual.TextStim(self.win, color=(1, 1, 1), pos=(0, 0), text="+", height=0.1)
-        self.stimulus = self.Stimulus(self.win)
+
+        self.instruction_text = visual.ImageStim(
+            win=self.win,
+            image="Images/color_task/instruction.png",
+            units="pix"
+        )
+
+        self.n_blocks = self.config["experiment"]["n_blocks"]
+        assert self.n_blocks <= 5, "Number of blocks should be less than or equal to 5"
+        self.blocks = []
+        for i in range(self.n_blocks):
+            self.blocks.append(visual.ImageStim(
+                win=self.win,
+                image="Images/color_task/block0{}.JPG".format(i + 1),
+                units="pix"
+            ))
+
+        # self.instruction_text = visual.TextStim(self.win, text="Press any key to start.")
+        self.fixation = visual.TextStim(self.win, color=(-1, -1, -1), pos=(0, 0), text="+",
+                                        height=self.config["experiment"]["fixation_size"])
+        self.stimulus = self.Stimulus(self.win, size=self.config["stimulus"]["size"],
+                                      distance=self.config["stimulus"]["distance"],
+                                      saturation=self.config["color_bar"]["saturation"],
+                                      value=self.config["color_bar"]["value"])
         # Setup Trial Handler
         # conditions = [{'text': 'Condition 1'}, {'text': 'Condition 2'}]
-        trials = self.config["experiment"]["trials"]
+        trials = self.config["experiment"]["n_trials"] * self.n_blocks
         conditions = []
         for i in range(trials):
-            colors = [random.random() for _ in range(4)]
+            colors = [random.random() for _ in range(3)]
             print(colors)
             conditions.append({'colors': colors})
+
         self.trials = data.TrialHandler(trialList=conditions, nReps=1, method='random')
+
+        self.total_trials = len(conditions)
+        self.trial_per_block = len(conditions) // self.n_blocks
+
         self.this_exp.addLoop(self.trials)  # This allows logging at each iteration
 
         # Clock for precise timing
@@ -79,8 +111,10 @@ class ColorTask:
         return self.config
 
     class ColorBar:
-        def __init__(self, win, width=1.2, height=0.02, pos=(0, -0.46), number_of_colors=200):
+        def __init__(self, win, width=1.2, height=0.02, pos=(0, -0.46), number_of_colors=200, saturation=1, value=1):
             self.win = win
+            self.saturation = saturation
+            self.value = value
 
             self.slider = visual.Slider(win=self.win, ticks=(0, 1), labels=['Red', 'Violet'],
                                         granularity=1 / number_of_colors,
@@ -97,7 +131,8 @@ class ColorTask:
             bar_start = pos[0] - (width / 2) + (bar_width / 2)
 
             for i in range(num_colors):
-                color = mpl_colors.hsv_to_rgb([i / num_colors, 1, 1])
+                color = mpl_colors.hsv_to_rgb([i / num_colors, self.saturation, self.value])
+                color = [(c * 2) - 1 for c in color]
                 rect = visual.Rect(win, width=bar_width, height=bar_height,
                                    pos=(bar_start + i * bar_width, pos[1] + height),
                                    fillColor=color,
@@ -109,7 +144,8 @@ class ColorTask:
             if self.slider.rating is not None:
                 # Map the slider value to a color in the spectrum
                 color_value = self.slider.getRating()  # This will be a value between 0 and 1
-                color = mpl_colors.hsv_to_rgb([color_value, 1, 1])  # Convert HSV to RGB
+                color = mpl_colors.hsv_to_rgb([color_value, self.saturation, self.value])
+                color = [(c * 2) - 1 for c in color]  # Convert HSV to RGB
 
                 # Update the feedback color
                 self.feedback.fillColor = color
@@ -128,32 +164,43 @@ class ColorTask:
             self.feedback.lineColor = 'white'
 
     class CircularColorBar:
-        def __init__(self, win, radius=0.5, pos=(0, 0), number_of_colors=200, feedback_radius=0.05):
+        def __init__(self, win, radius=0.5, pos=(0, 0), number_of_colors=200, feedback_radius=0.05, slider_radius=0.01,
+                     saturation=1, value=1, is_colorful=True):
             self.win = win
+            self.saturation = saturation
+            self.value = value
             self.radius = radius
             self.pos = pos
 
             self.num_colors = number_of_colors
             self.gradient_bar = []
 
-            self.feedback_circle = visual.Circle(self.win, fillColor='white', lineColor='white', radius=feedback_radius)
+            # self.feedback_object = (self.win, fillColor='white', lineColor='white', radius=feedback_radius)
+            self.feedback_object = visual.Rect(self.win, width=feedback_radius, height=feedback_radius,
+                                               fillColor=[0, 0, 0],
+                                               lineColor=[0, 0, 0])
 
             theta = np.linspace(0, 2 * np.pi, number_of_colors)
             for i in range(number_of_colors):
-                color = mpl_colors.hsv_to_rgb([i / number_of_colors, 1, 1])
+                if is_colorful:
+                    color = mpl_colors.hsv_to_rgb([i / number_of_colors, self.saturation, self.value])  #
+                    color = [(c * 2) - 1 for c in color]
+                else:
+                    color = [0.3, 0.3, 0.3]
                 x = pos[0] + radius * np.cos(theta[i])
                 y = pos[1] + radius * np.sin(theta[i])
-                circle = visual.Circle(win, radius=radius * 0.02,
+                circle = visual.Circle(win, radius=radius * 0.01,
                                        pos=(x, y),
-                                       fillColor=color,
-                                       lineColor=color)
+                                       fillColor=color,  # color,
+                                       lineColor=color, colorSpace='rgb')
                 self.gradient_bar.append(circle)
 
             # Create a feedback stimulus (e.g., a circle)
-            self.feedback = visual.Circle(self.win, radius=radius * 0.05, fillColor='white', lineColor='white',
+            self.feedback = visual.Circle(self.win, radius=slider_radius, fillColor='white', lineColor='white',
                                           pos=(pos[0] + radius, pos[1]))
 
             self.slider_pos = 0
+            self.feedback_started = False
 
         def draw(self):
             # Draw the gradient bar
@@ -162,14 +209,18 @@ class ColorTask:
 
             if self.slider_pos is not None:
                 # Map the slider position to a color in the spectrum
-                color_value = self.slider_pos / self.num_colors  # This will be a value between 0 and 1
-                color = mpl_colors.hsv_to_rgb([color_value, 1, 1])  # Convert HSV to RGB
+                color_value = self.slider_pos / self.num_colors # This will be a value between 0 and 1
+                if self.feedback_started:
 
+                    color = mpl_colors.hsv_to_rgb([color_value, self.saturation, self.value])  # Convert HSV to RGB
+                    color = [(c * 2) - 1 for c in color]
+                else:
+                    color = [0.3, 0.3, 0.3]
                 # Update the feedback color
                 self.feedback.fillColor = color
                 self.feedback.lineColor = color
-                self.feedback_circle.fillColor = color
-                self.feedback_circle.lineColor = color
+                self.feedback_object.fillColor = color
+                self.feedback_object.lineColor = color
 
                 # Update feedback position
                 theta = 2 * np.pi * color_value
@@ -178,11 +229,12 @@ class ColorTask:
 
             # Draw the feedback
             self.feedback.draw()
-            self.feedback_circle.draw()
+            self.feedback_object.draw()
 
         def incremental_update_slider(self, increment):
             # Update slider position based on increment
             self.slider_pos = (self.slider_pos + increment) % self.num_colors
+            self.feedback_started = True
 
         def update_slider(self, mouse_pos):
             # Update slider position based on mouse position
@@ -192,17 +244,19 @@ class ColorTask:
             if angle < 0:
                 angle += 2 * np.pi
             self.slider_pos = int(self.num_colors * angle / (2 * np.pi))
+            self.feedback_started = True
 
         def reset(self):
             self.slider_pos = 0
             self.feedback.fillColor = 'white'
             self.feedback.lineColor = 'white'
+            self.feedback_started = False
 
         def get_feedback_color(self, position):
 
             mouse = event.Mouse(win=self.win)
 
-            self.feedback_circle.pos = position
+            self.feedback_object.pos = position
 
             while True:
                 self.draw()
@@ -229,27 +283,45 @@ class ColorTask:
                 core.wait(0.01)
 
     class Stimulus:
-        def __init__(self, win, radius=0.05, distance=0.22):
+        def __init__(self, win, size=0.05, distance=0.22, saturation=1, value=1):
             self.win = win
+            self.saturation = saturation
+            self.value = value
             # Create a feedback stimulus (e.g., a circle)
-            self.circles = []
+            self.objects = []
 
-            # distances = [[-distance, -distance], [-distance, distance], [distance, -distance], [distance, distance]]
-            positions = [(-distance, -distance), (-distance, distance), (distance, -distance), (distance, distance)]
-            for i in range(4):
-                self.circles.append(visual.Circle(self.win, radius=radius, fillColor='white', lineColor='white',
-                                                  pos=positions[i]))
+            # # distances = [[-distance, -distance], [-distance, distance], [distance, -distance], [distance, distance]]
+            # positions = [(-distance, -distance), (-distance, distance), (distance, -distance), (distance, distance)]
+            # for i in range(4):
+            #     self.circles.append(visual.Circle(self.win, radius=radius, fillColor='white', lineColor='white',
+            #                                       pos=positions[i]))
+            # self.rectangles = []
+
+            # Define the positions for the rectangles
+            # Calculate positions for an equilateral triangle centered at (0, 0)
+            angle_offset = np.pi  # Start with one rectangle at the top
+            positions = []
+            for i in range(3):
+                angle = angle_offset + (i * 2 * np.pi / 3)  # 120 degrees apart
+                x = distance * np.cos(angle)
+                y = distance * np.sin(angle)
+                positions.append((x, y))
+
+            for pos in positions:
+                self.objects.append(
+                    visual.Rect(self.win, width=size, height=size, fillColor='white', lineColor='white', pos=pos))
 
         def draw(self, colors):
 
-            for i in range(4):
-                color = mpl_colors.hsv_to_rgb([colors[i], 1, 1])
-                self.circles[i].fillColor = color
-                self.circles[i].lineColor = color
-                self.circles[i].draw(win=self.win)
+            for i in range(len(self.objects)):
+                color = mpl_colors.hsv_to_rgb([colors[i], self.saturation, self.value])
+                color = [(c * 2) - 1 for c in color]
+                self.objects[i].fillColor = color
+                self.objects[i].lineColor = color
+                self.objects[i].draw(win=self.win)
 
         def get_position(self, index):
-            return self.circles[index].pos
+            return self.objects[index].pos
 
     def _draw_instruction(self):
         self.instruction_text.draw()
@@ -259,38 +331,48 @@ class ColorTask:
     def run(self):
 
         self._draw_instruction()
+        for block in range(self.n_blocks):
 
-        for trial in self.trials:
-            self.trial_clock.reset()  # Reset the clock at the start of each trial
-            self.color_bar.reset()
-            self.fixation.draw()
+            self.blocks[block].draw()
             self.win.flip()
-            core.wait(self.fixation_delay)  # Present the stimulus for 2 seconds
+            core.wait(2)
 
-            self.stimulus.draw(trial['colors'])
-            self.fixation.draw()
-            self.win.flip()
-            core.wait(self.stimulus_duration)  # Present the stimulus for 2 seconds
+            for _ in range(self.trial_per_block):
+                trial = self.trials.next()
+                if trial is None:
+                    break
 
-            # Get Response
-            # TODO: Start Here
-            index = random.randint(0, 4)
-            position = self.stimulus.get_position(index)
+                self.trial_clock.reset()  # Reset the clock at the start of each trial
+                self.color_bar.reset()
+                self.fixation.draw()
+                self.win.flip()
+                core.wait(self.fixation_delay)  # Present the stimulus for 2 seconds
 
-            # while not event.getKeys(keyList=["space", "escape"]):
-            answer = self.color_bar.get_feedback_color(position)
-            answer = mpl_colors.rgb_to_hsv(answer)
-            # self.fixation.draw()
-            # self.color_bar.draw()
-            # self.win.flip()
+                self.stimulus.draw(trial['colors'])
+                self.fixation.draw()
+                self.win.flip()
+                core.wait(self.stimulus_duration)  # Present the stimulus for 2 seconds
 
-            # Log data for the current trial
-            self.this_exp.addData('Stimulus', trial['colors'])
-            self.this_exp.addData('Stimulus Duration', 2)
-            self.this_exp.addData('Trial Start', self.trial_clock.getTime())
-            self.this_exp.addData('Feedback', answer)
-            self.this_exp.addData('Selected Position', index)
-            self.this_exp.nextEntry()
+                # Get Response
+                # TODO: Start Here
+                index = random.randint(0, 2)
+                position = self.stimulus.get_position(index)
+
+                # while not event.getKeys(keyList=["space", "escape"]):
+                answer = self.color_bar.get_feedback_color(position)
+                answer = mpl_colors.rgb_to_hsv(answer)
+                # answer = [(c * 2) - 1 for c in answer]
+                # self.fixation.draw()
+                # self.color_bar.draw()
+                # self.win.flip()
+
+                # Log data for the current trial
+                self.this_exp.addData('Stimulus', trial['colors'])
+                self.this_exp.addData('Stimulus Duration', 2)
+                self.this_exp.addData('Trial Start', self.trial_clock.getTime())
+                self.this_exp.addData('Feedback', answer)
+                self.this_exp.addData('Selected Position', index)
+                self.this_exp.nextEntry()
 
         # Clean up
         self.this_exp.saveAsWideText(self.filename + '.csv')
